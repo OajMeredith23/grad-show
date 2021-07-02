6df4ee13ce558c20577ba79f1c723ffbed4e664a
Oliver Meredith <mail@olivermeredith.com>
2021-07-02 12:58:12 +0100

    remove designer page

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,16 +51,17 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/designer.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-      },
-    })
-  })
 }
+
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.fields.slug,
+//       component: path.resolve(`./src/templates/designer.js`),
+//       context: {
+//         // Data passed to context is available
+//         // in page queries as GraphQL variables.
+//         slug: node.fields.slug,
+//       },
+//     })
+//   })
+// }
